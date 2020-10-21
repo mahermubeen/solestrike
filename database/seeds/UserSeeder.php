d9d686fc7998ccc8560b7b6d638d230931065fb2
Mahar Husnain <mahar.husnain@yahoo.com>
2020-10-22 00:56:36 +0500

    db seeder added for admin

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -1,21 +1,22 @@
 <?php
 
+namespace Database\Seeders;
+
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
-class DatabaseSeeder extends Seeder
+class UserSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
-
-         DB::table('users')->insert([
+        DB::table('users')->insert([
             'name' => 'admin',
             'is_admin' => '1',
             'email' => 'admin@gmail.com',
